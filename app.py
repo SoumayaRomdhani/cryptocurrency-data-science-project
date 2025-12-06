c50682c341c39060543e8212470046e75515c206
Cyrine Fk <sirinefakhfakh03@gmail.com>
2025-12-06 23:20:29 +0100

    descriptif

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 
 from descriptif.price_evolution import display_price_evolution
-from descriptif.volatility import display_volatility
 from descriptif.volume_analysis import display_volume_analysis
 
 # Chargement des données nettoyées
